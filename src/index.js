f0f89a97baf6a0839184533eb530ae733fa36247
Andrew Morris <voltrevo@gmail.com>
2016-05-13 18:18:08 +1000

    Write entry point with args (repo, fname)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const fs = require('fs-promise');
+
+const open = require('open');
+
+const issueToUrl = require('./issueToUrl.js');
+
+module.exports = (repo, fname) => (
+  fs.readFile(fname).then(issueStr => {
+    const url = issueToUrl(repo, fname, issueStr.toString());
+
+    return new Promise((resolve, reject) => {
+      open(url, err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  })
+);
