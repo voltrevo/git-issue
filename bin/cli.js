ed30a437d30ad3a35aa4195ed9afe7e3aeeeffcb
Andrew Morris <voltrevo@gmail.com>
2016-05-14 13:49:55 +1000

    Implement lookup of template names

--- bin/cli.js
+++ bin/cli.js
@@ -6,6 +6,7 @@
 
 const path = require('path');
 
+const afs = require('abstract-fs');
 const fs = require('fs-promise');
 const once = require('lodash/once');
 
@@ -58,9 +59,64 @@ const getRepo = once(() => { // eslint-disable-line arrow-body-style
   });
 });
 
+const resolveTemplateName = (templateName) => {
+  const pathObj = path.parse(templateName);
+
+  const tryDir = (dir) => afs.System.Dir(dir)
+    .contents()
+    .then(dirContents => {
+      const matches = dirContents.files.filter(dirFile => {
+        if (pathObj.ext) {
+          return dirFile === pathObj.base;
+        }
+
+        return path.parse(dirFile).name === pathObj.name;
+      });
+
+      if (matches.length > 1) {
+        throw new Error(
+          `Multiple matches found for ${templateName} in ${dir}: ${matches.join(', ')}`
+        );
+      }
+
+      if (matches.length === 0) {
+        return null;
+      }
+
+      return path.join(dir, matches[0]);
+    })
+  ;
+
+  const lookupPaths = ['.', path.join(process.env.HOME, '.git-issue', 'templates')];
+
+  const lookupLoop = (i) => {
+    if (i >= lookupPaths.length) {
+      return Promise.reject(new Error(`Template not found: ${templateName}`));
+    }
+
+    return tryDir(lookupPaths[i])
+      .then(match => {
+        if (match === null) {
+          return lookupLoop(i + 1);
+        }
+
+        return match;
+      })
+    ;
+  };
+
+  return lookupLoop(0);
+};
+
 Promise
-  .all(args._.map(fname =>
-    getRepo().then(repo => openIssue(repo, fname))
+  .all(args._.map(templateName =>
+    Promise.all([getRepo(), resolveTemplateName(templateName)])
+      .then(res => {
+        const repo = res[0];
+        const fname = res[1];
+
+        return openIssue(repo, fname);
+      })
   ))
   .catch(err => {
     console.error(err);
